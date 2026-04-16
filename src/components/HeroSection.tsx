07d2a93a3bab82ff3203e1473a030ebe8e1f9664
DakuTsuki <110971830+DakuTsuki@users.noreply.github.com>
2026-04-16 16:14:19 +0100

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import { ArrowRight } from "lucide-react";
-import heroImage from "@/assets/hero-image.png";
+import heroImage from "@/assets/hero-image.jpg";
 
 const HeroSection = () => {
   return (
